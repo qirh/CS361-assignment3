0d006b7bf57adb101e614e49e44b6f900f84fbe4
almto3 <almto3@hotmail.com>
2016-06-26 23:05:23 -0500

    done

--- src/Encoder.java
+++ src/Encoder.java
@@ -27,8 +27,8 @@ public class Encoder {
 	
 	/*	ASCII table list, must have 128 elements to work, will pad the first 97 and last n elements	*/
 	static ArrayList <Integer> num_frq = new ArrayList<Integer>();
+	static ArrayList <Integer> num_dbl_frq = new ArrayList<Integer>();
 	
-	static HuffmanTree ht = null;
 	
 	/*	increments the counter	*/
 	static void incrementAlphabet(int n){
@@ -48,8 +48,6 @@ public class Encoder {
 		LINES += n;
 	}
 	
-
-	
 	/*	converts Integer objects to int primitive types	*/
 	private static int[] convertIntegers(ArrayList<Integer> integers){
 	    int[] ret = new int[integers.size()];
@@ -60,9 +58,12 @@ public class Encoder {
 	    }
 	    return ret;
 	}
-	/*	returns the entropy	*/
-	static private double bitsPerSymbol(String encExt) throws Exception{
-		return ( 1.0 * Reader.readEncoding(encExt).length() ) / Reader.readFile().length();
+	/*	returns the entropy, flag = 1 symbol, !flag = false	*/
+	static private double bitsPerSymbol(String encExt, boolean flag) throws Exception{
+		if(flag)
+			return ( 1.0 * Reader.readEncoding(encExt).length() ) / Reader.readFile().length();
+		else
+			return ( 0.5 * Reader.readEncoding(encExt).length() ) / Reader.readFile().length();
 	}
 	
 	static private double diffrence(double x, double y){
@@ -78,7 +79,6 @@ public class Encoder {
 			if(y <= 0)
 				continue;
 			
-			//System.out.print("h = "+ h + " - " + "y = " + y + "\n");
 			h += ( y /ALPHABET_SIZE) * ( Math.log(y/ALPHABET_SIZE) / Math.log(2.0));
 		}
 		return -h;
@@ -95,13 +95,25 @@ public class Encoder {
 			num_frq.add(0);
 	}
 	
+	private static void setUpDoubleArray(String arg) throws Exception{
+		/* pad beginning with 97 zeros	*/
+		for (int i = 0; i < 97; i++)
+			num_frq.add(0);
+		/* add n chars	*/
+		Reader.readDoubleInput(arg, num_frq);
+		
+	}
+	
 	private static double round (double d, int i){
 		double out = d;
 		BigDecimal bd = new BigDecimal(out);
 		bd = bd.round(new MathContext(i));
 		return bd.doubleValue();
-		
-		
+	}
+	private static void clear(){
+		ALPHABET_SIZE = 0;
+		LINES = 0;
+		num_frq.clear();
 	}
 	
 	/*	Main method	*/
@@ -115,7 +127,7 @@ public class Encoder {
 	/* PART 2	*/
 		/* create huffman tree from frequencies	*/
 		System.out.println("PART 2");
-		ht = new HuffmanTree(convertIntegers(num_frq));
+		HuffmanTree ht = new HuffmanTree(convertIntegers(num_frq));
 		ht.displayChars();
 	
 	/* PART 3	*/
@@ -124,20 +136,38 @@ public class Encoder {
 		String encExt = ".enc1";
 		String decExt = ".dec1";
 		String output = Frequency.generateText(num_frq, ALPHABET_SIZE, k, encExt, decExt);
-		Writer.writeFile(output, encExt, decExt);
-		System.out.println("Generated and written " + k + " random charecters in testText, encoded them into testText" + encExt +" and decoded them in testText"+ decExt + "\n");
+		Writer.writeFile(ht, output, encExt, decExt, true);
+		System.out.println("Generated and written " + k + " random 1 symbol charecters in testText, encoded them into testText" + encExt +" and decoded them in testText"+ decExt + "\n");
 	
 	/* PART 4	*/
 		System.out.println("PART 4");
-		System.out.println("bits per symbol for " + k + " charecters: " + round(bitsPerSymbol(".enc1"), 3));
-		System.out.println("a " + round(diffrence(bitsPerSymbol(".enc1"), entropy() ), 3) + "% difference from entropy\n");
+		System.out.println("bits per symbol for " + k + " characters: " + round(bitsPerSymbol(encExt, true), 3));
+		double d1 = round(diffrence(bitsPerSymbol(encExt, true), entropy() ), 3);
+		System.out.println("a " + d1 + "% difference from entropy\n");
+		clear();
 		
 	/* PART 5	*/
-		System.out.println("PART 5");
+		System.out.println("PART 5&6");
+		setUpDoubleArray(args[0]);
 		encExt = ".enc2";
 		decExt = ".dec2";
 		output = Frequency.generateDoubleText(num_frq, ALPHABET_SIZE, k, encExt, decExt);
-		System.out.println(output);
+		ht = new HuffmanTree(convertIntegers(num_frq));
+		Writer.writeEncoding(ht, output, encExt, false);
+		Writer.writeDecoding(ht, encExt, decExt, false);
+		System.out.println("Generated " + k + " random 2 symbol charecters, encoded them into testText" + encExt +" and decoded them in testText"+ decExt);
+		System.out.println("bits per symbol for " + k + " characters: " + round(bitsPerSymbol(encExt, false), 3));
+		double d2 = round(diffrence(bitsPerSymbol(encExt, false), entropy() ), 3);
+		System.out.println("a " + d2 + "% difference from entropy\n");
+		
+	/* PART 7	*/
+		System.out.println("PART 7");
+		double d3 = round(d1-d2, 3);
+		if (d3 > 0)
+			System.out.println("a " + d3 + "% efficncy increase");
+		else
+			System.out.println("a " + d3 + "% efficncy decrease");
+	
 		
 	}
 }
@@ -161,51 +191,78 @@ class Writer {
 	}
 	
 	/*	write method will write to the file	*/
-	static void writeFile (String content, String encExt, String decExt) throws Exception{
+	static void writeFile (HuffmanTree ht, String content, String encExt, String decExt, boolean flag) throws Exception{
 		setPath();	
 		BufferedWriter writer = new BufferedWriter( new OutputStreamWriter( new FileOutputStream(filepath), "utf-8"));
 		writer.write(content);
 		writer.close();
 		
 		/*	encode the test and write it to testText.enc1	*/
-		writeEncoding(content, encExt);
+		writeEncoding(ht, content, encExt, flag);
 		
 		/*	decode the encoded version and write it to testText.dec1	*/
-		writeDecoding(encExt, decExt);
+		writeDecoding(ht, encExt, decExt, flag);
 	}
-	static private void writeEncoding (String content, String encExt) throws Exception{
+	// flag = 1 symbol encoding,!flag = 2 symbol encoding 
+	static void writeEncoding (HuffmanTree ht, String content, String encExt, boolean flag) throws Exception{
 		
-		
-		for (String line : Encoder.ht.returnChars().split("\n"))
-			//System.out.println("line --> " + line + "\t" + line.substring(3));
-			content = content.replace(line.substring(0, 1), line.substring(3));
+		if(flag)
+			for (String line : ht.returnChars().split("\n"))
+				content = content.replace(line.substring(0, 1), line.substring(3));
+		else{
+			for (String line : ht.returnChars().split("\n")){
+				content = content.replace(line.substring(0, 1), line.substring(3));
+			}
+		}
 		
 		BufferedWriter writer = new BufferedWriter( new OutputStreamWriter( new FileOutputStream(filepath+encExt), "utf-8"));
 		writer.write(content);
 		writer.close();
 	}
 	
-	static private void writeDecoding (String encExt, String decExt) throws Exception{
+	static void writeDecoding (HuffmanTree ht, String encExt, String decExt, boolean flag) throws Exception{
 		
 		String encodedText = Reader.readEncoding(encExt);
 		String decodedText = "";
-		String tmp = "";
+		StringBuilder tmp = new StringBuilder();
 		
 		ArrayList<String> encodings = new ArrayList<String>();
 		
 		for(int i = 0; i<Encoder.getLines(); i++){
-			encodings.add(Encoder.ht.returnChars().split("\n")[i].substring(3));
+			encodings.add(ht.returnChars().split("\n")[i].substring(3));
 		}
-		
-		for (int i = 0; i < encodedText.length(); i++){
-			tmp += encodedText.charAt(i);
-			for (int j = 0; j<encodings.size(); j++){
-				if(encodings.get(j).equals(tmp)){
-					decodedText += Encoder.ht.returnChars().split("\n")[j].substring(0,1);
-					tmp = "";
+		if(flag)
+			for (int i = 0; i < encodedText.length()-1; i++){
+				tmp.append( encodedText.charAt(i) );
+				for (int j = 0; j<encodings.size(); j++){
+					if(encodings.get(j).equals(tmp.toString())){
+						decodedText += ht.returnChars().split("\n")[j].substring(0,1);
+						tmp.setLength(0);;
+					}
 				}
 			}
+		else{
+			String[] tmpArray = ht.returnChars().split("\n");
+			String[] charArray = new String[Encoder.getLines()];
+			int c = 0;
+			for (int i = 0; i< Math.sqrt(Encoder.getLines()); i++)
+				for(int j = 0; j < Math.sqrt(Encoder.getLines()); j++){
+					charArray[c] = tmpArray[i].substring(0,1) + tmpArray[j].substring(0,1);
+					c++;
+				}
+			
+			for (int i = 0; i < encodedText.length()-1; i++){
+				tmp.append( encodedText.charAt(i) );
+				for (int j = 0; j<encodings.size(); j++)
+					if(encodings.get(j).equals(tmp.toString())){
+						decodedText += charArray[j];
+						tmp.setLength(0);;
+					}
+			}
+			for (int i = 0; i< charArray.length; i++)
+				System.out.println(charArray[i] + ": " + encodings.get(i));
 		}
+		
 		BufferedWriter writer = new BufferedWriter( new OutputStreamWriter( new FileOutputStream(filepath+decExt), "utf-8"));
 		writer.write(decodedText);
 		writer.close();
@@ -250,7 +307,7 @@ class Reader {
 	    	String line;
 		    br = new BufferedReader(new FileReader(input));
 		    while ((line = br.readLine()) != null) {
-		    	encoded .append(line);
+		    	encoded.append(line);
 		    }
 	    } 
 		catch (IOException e) {
@@ -283,6 +340,48 @@ class Reader {
 		    		Encoder.incrementLines(1);
 		    	}
 		    }
+	    } 
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
+		finally {
+			try {
+				if (br != null)
+					br.close();
+		    } 
+			catch (IOException ex) {
+				ex.printStackTrace();
+		    }
+		}
+	}
+	
+	/*	read method will read the input file	*/
+	static void readDoubleInput (String path, ArrayList <Integer> num_frq) throws Exception{
+		setPath(path);	
+	  
+	    try {
+	    	String tmp;
+	    	StringBuilder text = new StringBuilder();
+	    	
+		    br = new BufferedReader(new FileReader(input));
+		    br = new BufferedReader(new FileReader(input));
+		    int counter = 0;
+		    
+		    while ((tmp = br.readLine()) != null) {
+		    	text.append(tmp + "\n");
+		    }
+		    for (String line : text.toString().split("\n")){
+			    int n = Integer.parseInt(line);
+		    	if (n>0){
+		    		counter++;
+		    		for (String line2 : text.toString().split("\n")){
+		    			int n2 = Integer.parseInt(line2);
+		    			Encoder.incrementAlphabet(n*n2);
+		    			num_frq.add(n*n2);
+		    		}
+		    	}
+		    }
+		    Encoder.incrementLines(counter*counter);
 	    } 
 		catch (IOException e) {
 			e.printStackTrace();
--- src/Frequency.java
+++ src/Frequency.java
@@ -41,24 +41,29 @@ public class Frequency {
 	
 	static String generateDoubleText(ArrayList<Integer> occurances, int ALPHABET_SIZE, int k, String encExt, String decExt) throws Exception{
 		frq.clear();
+		
 		for (int x : occurances){
 			double w = x;
 			for (int z : occurances){
 				double y = z;
-				frq.add( (w / ALPHABET_SIZE) * (y/ALPHABET_SIZE) );
+				if(w > 0 && y > 0)
+					frq.add( (w / ALPHABET_SIZE) * (y/ALPHABET_SIZE) );
 			}
 		}
-		
 		HashMap<Range, String> freqmap = new HashMap<Range, String> ();
 		double sum = 0.0;
+		int counter = 0;
+		int counter2 = 0;
+		
 		for ( int i = 0; i < frq.size(); i++) { 
 		    double prob = 0.0;
-		    for (int j = 0; j < frq.size(); j++) {
-		    	prob = frq .get(i) * frq.get(j);
-		    	Range range = new Range( sum, sum + prob);
-		    	sum += prob;
-		    	freqmap.put(range, "" + (char) i + (char) j);
-		    }
+		    prob = frq .get(i);
+		    Range range = new Range( sum, sum + prob);
+		    sum += prob;
+		    freqmap.put(range, "" + ((char) ('a' + counter2%Math.sqrt(frq.size()))) + (char) ('a' + counter%Math.sqrt(frq.size())) );
+		    counter++;
+		    if(counter%Math.sqrt(frq.size()) == 0)
+		    	counter2++;
 		}
 
 		// This makes a random number from 0 to k and gives a letter depending on the range created above
@@ -72,30 +77,12 @@ public class Frequency {
 				if (range.contains((1.0*prob)/k)){
 					if(freqmap.get(range).equals('#'))
 						throw new Exception("EQUALS --> " + (1.0*prob)/k);
-					System.out.println("HERE1");
+					//System.out.print("HERE1  -- " + freqmap.get(range));
 					sb.append(freqmap.get(range));
 				}
 			}
 		}
 		return sb.toString();
-//		
-//		// This makes a random number from 0 to k and gives a letter depending on the range created above
-//		Random randomGenerator = new Random();
-//		// The 10 below is your k characters that you need to generate
-//		String output = "";
-//		for (int i = 0; i < k; i++){
-//			int prob = randomGenerator.nextInt(k);
-//			for(Range range: freqmap.keySet()){
-//				if (range.contains((1.0*prob)/k)){
-//					if(freqmap.get(range).equals('#'))
-//						throw new Exception("EQUALS --> " + (1.0*prob)/k);
-//					System.out.println("--" + freqmap.get(range));
-//					output += freqmap.get(range);
-//				}
-//			}
-//		}
-//		return output;
-		
 	}
 }
 
